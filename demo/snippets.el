656d3fca77e24485651fd4d64f396d9d2e754833
sopier <sopier@gmail.com>
2015-01-30 20:25:42 +0700

    adding xahlee nice html tricks

--- demo/snippets.el
+++ demo/snippets.el
@@ -57,3 +57,64 @@
 
 ; line_1 abcdefghij
 ; line_2 klmnopqrst
+
+
+;------------------------------------------------;
+;;; xahlee turn block into html list           ;;;
+;;; https://github.com/xahlee/xah-html-mode.el ;;;
+;------------------------------------------------;
+
+(defun get-selection-or-unit (unit)
+  "Return the string and boundary of text selection or UNIT under cursor.
+
+If `use-region-p' is true, then the region is the unit.  Else,
+it depends on the UNIT. See `unit-at-cursor' for detail about
+UNIT.
+
+Returns a vector [text a b], where text is the string and a and b
+are its boundary.
+
+Example usage:
+ (setq bds (get-selection-or-unit 'line))
+ (setq inputstr (elt bds 0) p1 (elt bds 1) p2 (elt bds 2)  )"
+  (interactive)
+
+  (let ((p1 (region-beginning)) (p2 (region-end)))
+    (if (use-region-p)
+        (vector (buffer-substring-no-properties p1 p2) p1 p2 )
+      (unit-at-cursor unit) ) ) )
+
+(defun xhm-lines-to-html-list ()
+  (interactive)
+  (let (bds p1 p2 ξinput-str resultStr)
+    (setq bds (get-selection-or-unit 'block))
+    (setq ξinput-str (elt bds 0) p1 (elt bds 1) p2 (elt bds 2))
+    (save-excursion
+      (setq resultStr
+            (with-temp-buffer
+              (insert ξinput-str)
+              (delete-trailing-whitespace)
+              (goto-char 1)
+              (while
+                  (search-forward-regexp  "\.html$" nil t)
+                (backward-char 1)
+                (xah-all-linkify))
+
+              (goto-char 1)
+              (while
+                  (not (equal (line-end-position) (point-max)))
+                (beginning-of-line) (insert "<li>")
+                (end-of-line) (insert "</li>")
+                (forward-line 1 ))
+
+              (beginning-of-line) (insert "<li>")
+              (end-of-line) (insert "</li>")
+
+              (goto-char 1)
+              (insert "<ul>\n")
+              (goto-char (point-max))
+              (insert "\n</ul>")
+
+              (buffer-string))))
+    (delete-region p1 p2)
+    (insert resultStr)))
